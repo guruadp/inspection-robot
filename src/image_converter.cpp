9bc180f4b5480505f556f9b7628b64e08c4fae9b
RR-Vignesh <vignesh31794@gmail.com>
2022-12-16 20:50:05 -0500

    3.7.5 bug fix for open cv

--- src/image_converter.cpp
+++ src/image_converter.cpp
@@ -43,15 +43,15 @@
 
 using namespace std::chrono_literals;
 
-#include <opencv2/opencv.hpp>
+// #include <opencv2/opencv.hpp>
 
 // #include <sensor_msgs/msg/image_encodings.h>
 #include <iostream>
 // #include <opencv2/aruco.hpp>
-#include <opencv2/highgui.hpp>
+/* #include <opencv2/highgui.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgproc/imgproc.hpp> */
 
 #include "rclcpp/rclcpp.hpp"
 using std::placeholders::_1;
